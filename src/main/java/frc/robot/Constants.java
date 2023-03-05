cd71b8a73eb7e9b1bfa2edc121b3f8e40104e861
GovindRobo <buttargovind@gmail.com>
2023-03-05 14:37:50 -0800

    Vision Subsystem

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -210,5 +210,73 @@ public final class Constants {
   public static Mode getMode() {
       return Mode.REAL;
   }
+  public static class VisionConstants {
+    public static final double H1 = 0.279; // METERS, 0.3429 on board, 0.489 on floudner.
+    public static final double H2a = 1.1736; //Low goal = 0.628, high goal = 1.17
+    public static final double H2b = 0.638; //height of low goal
+    public static final double A1 = 20; // limelight angle!
+
+    public static final double retroPipeline = 1; //pipline id of the retroreflective pipeline.
+    
+    public static final double C1 = 0; //vrt / y value check the picture Idk why govind called it that
+    public static final double C2 = 0; // horizontal / x value check the picture :??????
+
+    public static class Field {
+    public static final float FIELD_WIDTH = 16.54175f;
+    public static final float FIELD_HEIGHT = 8.0137f;
+    public static final int NUM_TAGS = 8;
+    public static final Point[] TAGS = {
+        new Point(Tags.ID_1.x, Tags.ID_1.y),
+        new Point(Tags.ID_2.x, Tags.ID_2.y),
+        new Point(Tags.ID_3.x, Tags.ID_3.y),
+        new Point(Tags.ID_4.x, Tags.ID_4.y),
+        new Point(Tags.ID_5.x, Tags.ID_5.y),
+        new Point(Tags.ID_6.x, Tags.ID_6.y),
+        new Point(Tags.ID_7.x, Tags.ID_7.y),
+        new Point(Tags.ID_8.x, Tags.ID_8.y)
+    };
+    }
+    
+}
+public static enum Tags {
+    ID_1(15.513558f, 1.071626f, 0.462788f),
+    ID_2(15.513558f, 2.748026f, 0.462788f),
+    ID_3(15.513558f, 4.424426f, 0.462788f),
+    ID_4(16.178784f, 6.749796f, 0.695452f),
+    ID_5(0.36195f, 6.749796f, 0.695452f),
+    ID_6(1.02743f, 4.424426f, 0.462788f),
+    ID_7(1.02743f, 2.748026f, 0.462788f),
+    ID_8(1.02743f, 1.071626f, 0.462788f);
+
+    public final float x;
+    public final float y;
+    public final float z;
+
+    Tags(float x, float y, float z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
+public static class Vision {
+    public static final int LED = 3;
+    public static final double H1 = 5; // distance between limelight and ground (height of limelight mount)
+    public static final double H2 = 107; // height of target
+    public static final double A1 = 35; // angle from horizontal axis
+    public static final double AREA_VISIBLE = 1; //if area is large enough to be visible (ta)
+    
+    public static class Distance {
+        public static final int STATE1 = 110; 
+        public static final int STATE2 = 150;
+        public static final int STATE3 = 190;
+        public static final int STATE4 = 270;
+    }
+
+    public static final double apriltagToMidHorizontal = 0.219837;
+    public static final double apriltagToHighHorizontal = apriltagToMidHorizontal*3;
+    public static final double apriltagtoMidVertical = 0.15875;
+    public static final double apriltagtoHighVertical = 0.73025;
+
+}
 
 }
---
+++ src/main/java/frc/robot/subsystems/Vision/VisionSubsystem.java
@@ -0,0 +1,291 @@
+package frc.robot.subsystems.Vision;
+
+import java.util.HashMap;
+
+import org.littletonrobotics.junction.Logger;
+
+import edu.wpi.first.math.filter.LinearFilter;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.networktables.DoubleEntry;
+import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.util.wrappers.VisionMeasurement;
+import frc.robot.subsystems.Vision.LimelightHelpers;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.shuffleboard.SimpleWidget;
+import edu.wpi.first.networktables.NetworkTableEntry;
+
+public class VisionSubsystem extends SubsystemBase {
+
+    // private final double filterTimeConstant = 0.02;
+    // private final double filterPeriod = 0.005;
+    private final HashMap<Integer, LinearFilter> linearFilters = new HashMap<>(){{
+        /* put(0, LinearFilter.singlePoleIIR(filterTimeConstant, filterPeriod));
+        put(1, LinearFilter.singlePoleIIR(filterTimeConstant, filterPeriod));
+        put(2, LinearFilter.singlePoleIIR(filterTimeConstant, filterPeriod));
+        put(3, LinearFilter.singlePoleIIR(filterTimeConstant, filterPeriod));
+        put(4, LinearFilter.singlePoleIIR(filterTimeConstant, filterPeriod));
+        put(5, LinearFilter.singlePoleIIR(filterTimeConstant, filterPeriod));
+        put(6, LinearFilter.singlePoleIIR(filterTimeConstant, filterPeriod));
+        put(7, LinearFilter.singlePoleIIR(filterTimeConstant, filterPeriod)); */
+        put(0, LinearFilter.movingAverage(10));
+        put(1, LinearFilter.movingAverage(10));
+        put(2, LinearFilter.movingAverage(10));
+        put(3, LinearFilter.movingAverage(10));
+        put(4, LinearFilter.movingAverage(10));
+        put(5, LinearFilter.movingAverage(10));
+        put(6, LinearFilter.movingAverage(10));
+        put(7, LinearFilter.movingAverage(10));
+    }};
+
+    private NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+    private NetworkTable ll2 = NetworkTableInstance.getDefault().getTable("limelight-two");
+    private NetworkTable ll3 = NetworkTableInstance.getDefault().getTable("limelight-three");  
+
+    private double[] rawDistances = new double[]{
+        -1,
+        -1,
+        -1,
+        -1,
+        -1,
+        -1,
+        -1,
+        -1
+    };
+    private double[] distances = new double[]{
+        -1,
+        -1,
+        -1,
+        -1,
+        -1,
+        -1,
+        -1,
+        -1
+    };
+
+    public VisionSubsystem() {}
+
+    private NetworkTable getTable(){
+        if (ll3.getEntry("tv").getInteger(0) == 1){
+            return ll3;
+        } else {
+            return ll2;
+        }
+    }
+
+    private Pose2d getCamPose2d() {
+        double id = getTagID();
+        for (var v : getLatestResults().targetingResults.targets_Fiducials) {
+            if (v.fiducialID == id) return v.getRobotPose_TargetSpace2D();
+        }
+
+        return new Pose2d();
+    }
+
+    public long getPipeline(){
+        return getTable().getEntry("getpipe").getInteger(0);
+    }
+
+    private double[] getCamTran() {
+        double id = getTagID();
+        for (var v : getLatestResults().targetingResults.targets_Fiducials) {
+            if (v.fiducialID == id) return new double[]{
+                v.getRobotPose_TargetSpace2D().getX(),
+                v.getRobotPose_TargetSpace2D().getY(),
+                v.getRobotPose_TargetSpace2D().getRotation().getDegrees()
+            };
+        }
+
+        double[] pose = table.getEntry("botpose_targetspace").getDoubleArray(new double[10]);
+        if (pose.length > 0) return pose;
+        else return new double[6];
+    }
+
+    public boolean hasTargets() {
+        return table.getEntry("tv").getInteger(0) == 1;
+    }
+
+    /* Retroreflective(NOT USEFUL) */
+
+    public double getTX(){
+        return table.getEntry("tx").getDouble(0.0);
+    }
+
+    private double getTY(){
+        return table.getEntry("ty").getDouble(0.0);
+    }
+
+    private double getDistanceFromRetro(){
+        if (getTY() < -3){
+            return 39.37 * (Constants.VisionConstants.H2b - Constants.VisionConstants.H1) / Math.tan(0.017453292519943295 * (Constants.VisionConstants.A1 + getTY()));
+        } 
+        return 39.37 * (Constants.VisionConstants.H2a - Constants.VisionConstants.H1) / Math.tan(0.017453292519943295 * (Constants.VisionConstants.A1 + getTY()));
+    }
+
+    public double[] getOffsetTo2DOFBase(){        
+
+        double d1 = getDistanceFromRetro();
+        double tx = getTX();
+
+        double a = Math.sin(Math.toRadians(tx)) * d1;  // x val 
+        double b = Math.cos(Math.toRadians(tx)) * d1;  // y val
+
+        double angle = Math.atan((a + Constants.VisionConstants.C2) / (b + Constants.VisionConstants.C1));
+
+        double[] x = {a + Constants.VisionConstants.C2, b + Constants.VisionConstants.C1, Math.toDegrees(angle)};
+
+        return x;
+    }
+
+    /**
+     * *
+     * @param pipelineNum the pipeline number to switch to. We might need one for the high tape, one for the mid tape and one for apriltag.
+     */
+    public void switchPipeline(int pipelineNum){
+        table.getEntry("pipeline").setNumber(pipelineNum);
+    }
+    /* End Retroreflective */
+
+    private int getTagID() {
+        return (int) table.getEntry("tid").getInteger(-1);
+    }
+
+    private double[] getRawDistances() {
+        rawDistances = new double[]{
+            -1,
+            -1,
+            -1,
+            -1,
+            -1,
+            -1,
+            -1,
+            -1
+        };
+
+        for (var r : getLatestResults().targetingResults.targets_Fiducials) {
+            var tmp = r.getRobotPose_TargetSpace();
+            rawDistances[(int) r.fiducialID - 1] = Math.hypot(tmp.getX(), tmp.getZ());
+        }
+
+        return rawDistances;
+    }
+
+    private double[] getDistances() {
+        getRawDistances();
+        for (int i = 0; i < rawDistances.length; ++i) {
+            /* if (rawDistances[i] > 0)  */distances[i] = linearFilters.get(i).calculate(distances[i]);
+            /* else {
+                distances[i] = -1;
+                linearFilters.get(i).calculate(rawDistances[i]);
+            } */
+        }
+
+        return distances;
+    }
+
+    public VisionMeasurement getLatestMeasurement() {
+        LimelightResults results = getLatestResults();
+        return new VisionMeasurement(
+            hasTargets(),
+            results.targetingResults.latency_capture + 
+                results.targetingResults.latency_pipeline + 
+                results.targetingResults.latency_jsonParse,
+            getTagID(),
+            results.targetingResults.getBotPose2d(),
+            getCamPose2d(),
+            getRawDistances() // FIXME filtering distances
+        );
+    }
+
+    public LimelightResults getLatestResults() {
+        return LimelightHelpers.getLatestResults("");
+    }
+
+/* START OF ATREY'S APRILTAG CODE; USING OLD FUNCTIONS; RETURNS TX, TY, CAMERA-RELATIVE ANGLE TO APRILTAG */
+
+    private double[] getCamTranOld() {
+        return table.getEntry("botpose_targetspace").getDoubleArray(new double[6]);
+    }
+
+    public double getLimelightAngle(){
+        /* double rawAngle = getLatestResults().targetingResults.getBotPose2d().getRotation().getDegrees();
+         double finalAngle=0;
+        if(90<rawAngle && rawAngle<180){
+            finalAngle = rawAngle-90;
+        }
+        else if(-180<rawAngle && rawAngle<-90){
+            finalAngle=rawAngle+270;
+        }
+        else{
+            finalAngle=-1;
+        }
+        return finalAngle; */
+        return getLatestResults().targetingResults.getBotPose2d().getRotation().getDegrees();
+    }
+
+    public double getLimelightTx(int topOrMid){
+        double apriltagDistance = Math.hypot(getCamTranOld()[0], getCamTranOld()[2]);
+
+        if(topOrMid==1){
+            return apriltagDistance+Constants.Vision.apriltagToMidHorizontal;
+        }
+        else if(topOrMid==2){
+            return apriltagDistance+Constants.Vision.apriltagToHighHorizontal;
+        }
+        else{
+            return -1;
+        }
+    }
+
+    public double getLimelightTy(int topOrMid){
+        if(topOrMid==1){
+            return 0.6477;
+        }
+        else if(topOrMid==2){
+            return 0.9652;
+        }
+        else{
+            return -1;
+        }
+    }
+
+    @Override
+    public void periodic() {
+
+        // Logger.getInstance().recordOutput("Smart Targeting X", 100*Math.hypot(getCamTranOld()[0], getCamTranOld()[2])); //get X stuff for verification
+        // SmartDashboard.putNumberArray("Botpose 2d", getLatestResults().targetingResults.botpose);
+        // SmartDashboard.putNumberArray("Distances", getDistances());
+        SmartDashboard.putNumberArray("Pose to target(arm base)", getOffsetTo2DOFBase()); //Ignore if not on retroreflective pipeline. 
+        SmartDashboard.putNumber("distance to retro", getDistanceFromRetro());
+
+
+        // SmartDashboard.putNumber("Raw Angle", getLatestResults().targetingResults.getBotPose2d().getRotation().getDegrees());
+
+
+        // SmartDashboard.putNumber("LL to Mid (Horizontal)",getLimelightTx(1));
+        // SmartDashboard.putNumber("LL to High (Horizontal)", getLimelightTx(2));
+        // SmartDashboard.putNumber("LL to Mid (Vertical)",getLimelightTy(1));
+        // SmartDashboard.putNumber("LL to High (Vertical)",getLimelightTy(2));
+        // SmartDashboard.putNumber("LL to AprilTag",Math.hypot(getCamTranOld()[0], getCamTranOld()[2]));
+        NetworkTableInstance.getDefault().flush();
+        //meters to inches: 1m = 39.3701 in
+        SmartDashboard.putNumber("LL Angle",getLimelightAngle());
+        SmartDashboard.putNumber("LL to Mid (Horizontal, Inches)",39.3701*(getLimelightTx(1)));
+        SmartDashboard.putNumber("LL to High (Horizontal, Inches)", 39.3701*(getLimelightTx(2)));
+        SmartDashboard.putNumber("LL to Mid (Vertical, Inches)",39.3701*(getLimelightTy(1)));
+        SmartDashboard.putNumber("LL to High (Vertical, Inches)",39.3701*(getLimelightTy(2)));
+        SmartDashboard.putNumber("LL to AprilTag (Inches)",39.3701*(Math.hypot(getCamTranOld()[0], getCamTranOld()[2])));
+        SmartDashboard.putNumber("getTX", getTX());
+        NetworkTableInstance.getDefault().flush();
+    }
+
+    @Override
+    public void simulationPeriodic() {
+        
+    }
+}
