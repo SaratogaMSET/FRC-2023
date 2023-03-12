154046a8facff4562b05e76c9f3300588e09220d
GovindRobo <buttargovind@gmail.com>
2023-03-12 14:02:24 -0700

    Add waypoints for arm motion

--- src/main/java/frc/robot/commands/Arm/ArmSequences.java
+++ src/main/java/frc/robot/commands/Arm/ArmSequences.java
@@ -1,5 +1,7 @@
 package frc.robot.commands.Arm;
 
+import java.util.function.IntSupplier;
+
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants;
@@ -11,14 +13,151 @@ public class ArmSequences{
         ArmPositionCommand ready;
         ArmPositionCommand intake;
         if(side > 0){
-            ready = new ArmPositionCommand(armSubsystem, Constants.ArmNodeDictionary.ready_ground_intake_x, Constants.ArmNodeDictionary.ready_ground_intake_y);
-            intake = new ArmPositionCommand(armSubsystem, Constants.ArmNodeDictionary.ground_intake_x, Constants.ArmNodeDictionary.ground_intake_y);
-        }else{
             ready = new ArmPositionCommand(armSubsystem, -Constants.ArmNodeDictionary.ready_ground_intake_x, Constants.ArmNodeDictionary.ready_ground_intake_y);
             intake = new ArmPositionCommand(armSubsystem, -Constants.ArmNodeDictionary.ground_intake_x, Constants.ArmNodeDictionary.ground_intake_y);
+        }else{
+            ready = new ArmPositionCommand(armSubsystem, Constants.ArmNodeDictionary.ready_ground_intake_x, Constants.ArmNodeDictionary.ready_ground_intake_y);
+            intake = new ArmPositionCommand(armSubsystem, Constants.ArmNodeDictionary.ground_intake_x, Constants.ArmNodeDictionary.ground_intake_y);
         }
         ArmZeroCommand zero = new ArmZeroCommand(armSubsystem);
 
         return ready.andThen(intake).andThen(zero);
-    }         
+    }  
+    
+    public static SequentialCommandGroup autonGroundIntake(ArmSubsystem armSubsystem, int side){
+        ArmPositionCommand intake;
+        if(side > 0){
+            intake = new ArmPositionCommand(armSubsystem, -(Constants.ArmNodeDictionary.ground_intake_x + 0.1524), Constants.ArmNodeDictionary.ground_intake_y + (0.1524/2));
+        }else{
+            intake = new ArmPositionCommand(armSubsystem, Constants.ArmNodeDictionary.ground_intake_x +  0.1524, Constants.ArmNodeDictionary.ground_intake_y + (0.1524/2));
+        }
+        ArmZeroCommand zero = new ArmZeroCommand(armSubsystem);
+        return intake.andThen(zero);
+    }
+
+    public static SequentialCommandGroup groundIntakeNoRetract(ArmSubsystem armSubsystem, int side){
+        ArmPositionCommand ready;
+        ArmPositionCommand intake;
+        if(side > 0){
+            ready = new ArmPositionCommand(armSubsystem, -Constants.ArmNodeDictionary.ready_ground_intake_x, Constants.ArmNodeDictionary.ready_ground_intake_y);
+            intake = new ArmPositionCommand(armSubsystem, -Constants.ArmNodeDictionary.ground_intake_x, Constants.ArmNodeDictionary.ground_intake_y);
+        }else{
+            ready = new ArmPositionCommand(armSubsystem, Constants.ArmNodeDictionary.ready_ground_intake_x, Constants.ArmNodeDictionary.ready_ground_intake_y);
+            intake = new ArmPositionCommand(armSubsystem, Constants.ArmNodeDictionary.ground_intake_x, Constants.ArmNodeDictionary.ground_intake_y);
+        }
+        ArmZeroCommand zero = new ArmZeroCommand(armSubsystem);
+
+        return ready.andThen(intake).andThen(zero);
+    }   
+
+    // public static ArmPositionCommand ready(ArmSubsystem armSubsystem, IntSupplier side){
+    //     ArmPositionCommand ready;
+    //     if(side.getAsInt() > 0){
+    //         ready = new ArmPositionCommand(armSubsystem, -Constants.ArmNodeDictionary.ready_double_substation_x, Constants.ArmNodeDictionary.ready_double_substation_y);
+    //     }else{
+    //         ready = new ArmPositionCommand(armSubsystem, Constants.ArmNodeDictionary.ready_double_substation_x, Constants.ArmNodeDictionary.ready_double_substation_y);
+    //     }
+
+    //     return ready;
+    // }
+    public static ArmPositionCommand ready(ArmSubsystem armSubsystem, int side){
+        ArmPositionCommand ready;
+        if(side > 0){
+            ready = new ArmPositionCommand(armSubsystem, -Constants.ArmNodeDictionary.ready_double_substation_x, Constants.ArmNodeDictionary.ready_double_substation_y);
+        }else{
+            ready = new ArmPositionCommand(armSubsystem, Constants.ArmNodeDictionary.ready_double_substation_x, Constants.ArmNodeDictionary.ready_double_substation_y);
+        }
+
+        return ready;
+    }
+
+    public static SequentialCommandGroup lowScore(ArmSubsystem armSubsystem, int side){
+        ArmPositionCommand ready;
+        ArmPositionCommand score;
+        if(side > 0){
+            ready = new ArmPositionCommand(armSubsystem, -Constants.ArmNodeDictionary.ready_ground_intake_x, Constants.ArmNodeDictionary.ready_ground_intake_y);
+            score = new ArmPositionCommand(armSubsystem, -Constants.ArmNodeDictionary.ready_low_score_x, Constants.ArmNodeDictionary.ready_low_score_y);
+        }else{
+            ready = new ArmPositionCommand(armSubsystem, Constants.ArmNodeDictionary.ready_double_substation_x, Constants.ArmNodeDictionary.ready_double_substation_y);
+            score = new ArmPositionCommand(armSubsystem, Constants.ArmNodeDictionary.ready_low_score_x, Constants.ArmNodeDictionary.ready_low_score_y);
+        }
+        ArmZeroCommand zero = new ArmZeroCommand(armSubsystem);
+
+        return ready.andThen(score).andThen(zero);
+    }
+
+    public static SequentialCommandGroup scoreCubeMid(ArmSubsystem armSubsystem, int side){
+        ArmPositionCommand ready;
+        ArmPositionCommand score;
+        if(side > 0){
+            ready = new ArmPositionCommand(armSubsystem, -Constants.ArmNodeDictionary.ready_double_substation_x, Constants.ArmNodeDictionary.ready_double_substation_y);
+            score = new ArmPositionCommand(armSubsystem, -Constants.ArmNodeDictionary.ready_midcube_score_x, Constants.ArmNodeDictionary.ready_midcube_score_y);
+        }else{
+            ready = new ArmPositionCommand(armSubsystem, Constants.ArmNodeDictionary.ready_double_substation_x, Constants.ArmNodeDictionary.ready_double_substation_y);
+            score = new ArmPositionCommand(armSubsystem, Constants.ArmNodeDictionary.ready_midcube_score_x, Constants.ArmNodeDictionary.ready_midcube_score_y);
+        }
+        ArmZeroCommand zero = new ArmZeroCommand(armSubsystem);
+
+        return ready.andThen(score).andThen(zero);
+    }
+
+    public static SequentialCommandGroup scoreCubeHigh(ArmSubsystem armSubsystem, int side){
+        ArmPositionCommand ready;
+        ArmPositionCommand score;
+        if(side > 0){
+            ready = new ArmPositionCommand(armSubsystem, -Constants.ArmNodeDictionary.ready_double_substation_x, Constants.ArmNodeDictionary.ready_double_substation_y);
+            score = new ArmPositionCommand(armSubsystem, -Constants.ArmNodeDictionary.ready_highcube_score_x, Constants.ArmNodeDictionary.ready_highcube_score_y);
+        }else{
+            ready = new ArmPositionCommand(armSubsystem, Constants.ArmNodeDictionary.ready_double_substation_x, Constants.ArmNodeDictionary.ready_double_substation_y);
+            score = new ArmPositionCommand(armSubsystem, Constants.ArmNodeDictionary.ready_highcube_score_x, Constants.ArmNodeDictionary.ready_highcube_score_y);
+        }
+        ArmZeroCommand zero = new ArmZeroCommand(armSubsystem);
+
+        return ready.andThen(score).andThen(zero);
+    }
+
+    public static SequentialCommandGroup scoreCubeHighNoRetract(ArmSubsystem armSubsystem, int side){
+        ArmPositionCommand ready;
+        ArmPositionCommand score;
+        if(side > 0){
+            ready = new ArmPositionCommand(armSubsystem, -Constants.ArmNodeDictionary.ready_double_substation_x, Constants.ArmNodeDictionary.ready_double_substation_y);
+            score = new ArmPositionCommand(armSubsystem, -Constants.ArmNodeDictionary.ready_highcube_score_x, Constants.ArmNodeDictionary.ready_highcube_score_y);
+        }else{
+            ready = new ArmPositionCommand(armSubsystem, Constants.ArmNodeDictionary.ready_double_substation_x, Constants.ArmNodeDictionary.ready_double_substation_y);
+            score = new ArmPositionCommand(armSubsystem, Constants.ArmNodeDictionary.ready_highcube_score_x, Constants.ArmNodeDictionary.ready_highcube_score_y);
+        }
+        ArmZeroCommand zero = new ArmZeroCommand(armSubsystem);
+
+        return ready.andThen(score);
+    }
+
+    public static SequentialCommandGroup scoreConeMid(ArmSubsystem armSubsystem, int side){
+        ArmPositionCommand ready;
+        ArmPositionCommand score;
+        if(side > 0){
+            ready = new ArmPositionCommand(armSubsystem, -Constants.ArmNodeDictionary.ready_double_substation_x, Constants.ArmNodeDictionary.ready_double_substation_y);
+            score = new ArmPositionCommand(armSubsystem, -Constants.ArmNodeDictionary.ready_midcone_score_x, Constants.ArmNodeDictionary.ready_midcone_score_y);
+        }else{
+            ready = new ArmPositionCommand(armSubsystem, Constants.ArmNodeDictionary.ready_double_substation_x, Constants.ArmNodeDictionary.ready_double_substation_y);
+            score = new ArmPositionCommand(armSubsystem, Constants.ArmNodeDictionary.ready_midcone_score_x, Constants.ArmNodeDictionary.ready_midcone_score_y);
+        }
+        ArmZeroCommand zero = new ArmZeroCommand(armSubsystem);
+
+        return ready.andThen(score).andThen(zero);
+    }
+
+    public static SequentialCommandGroup scoreConeHigh(ArmSubsystem armSubsystem, int side){
+        ArmPositionCommand ready;
+        ArmPositionCommand score;
+        if(side > 0){
+            ready = new ArmPositionCommand(armSubsystem, -Constants.ArmNodeDictionary.ready_double_substation_x, Constants.ArmNodeDictionary.ready_double_substation_y);
+            score = new ArmPositionCommand(armSubsystem, -Constants.ArmNodeDictionary.ready_highcone_score_x, Constants.ArmNodeDictionary.ready_highcone_score_y);
+        }else{
+            ready = new ArmPositionCommand(armSubsystem, Constants.ArmNodeDictionary.ready_double_substation_x, Constants.ArmNodeDictionary.ready_double_substation_y);
+            score = new ArmPositionCommand(armSubsystem, Constants.ArmNodeDictionary.ready_highcone_score_x, Constants.ArmNodeDictionary.ready_highcone_score_y);
+        }
+        ArmZeroCommand zero = new ArmZeroCommand(armSubsystem);
+
+        return ready.andThen(score).andThen(zero);
+    }
 }
