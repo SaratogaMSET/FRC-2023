048fa0f2adbfbf026b5c0a315cbb2221f4de84f5
GovindRobo <buttargovind@gmail.com>
2023-04-11 10:06:40 -0700

    Update Closest 90 command

--- src/main/java/frc/robot/commands/Drivetrain/MoveWithClosest90.java
+++ src/main/java/frc/robot/commands/Drivetrain/MoveWithClosest90.java
@@ -11,109 +11,160 @@ import frc.robot.Constants.ArmNodeDictionary;
 import frc.robot.subsystems.Drivetrain.DrivetrainSubsystem;
 
 public class MoveWithClosest90 extends CommandBase {
+    private final DrivetrainSubsystem m_drivetrainSubsystem;
 
+    private final DoubleSupplier m_translationXSupplier;
+    private final DoubleSupplier m_translationYSupplier;
+    private final DoubleSupplier gunnerSupplier;
+    private final DoubleSupplier armHeight;
+    private final DoubleSupplier actuatorHeight;
+    private double m_translationXTrapezoidal = 0;
+    private double m_translationYTrapezoidal = 0;
+    double translationalScaler = 0.5;
     double desiredAngle;
-    private DrivetrainSubsystem drivetrain;
-    DoubleSupplier x;
-    DoubleSupplier y;
-    DoubleSupplier armHeight;
-    DoubleSupplier actuatorHeight;
-    PIDController controller = new PIDController(2,0.0,0.0);
-    double xT;
-    double yT;
     int axis = 0;
     double lastRot;
+    private PIDController controller = new PIDController(10, 0,0); //3.5
     double minArmValue = 0.3; 
     double maxArmValue = 1.18; 
+
     public MoveWithClosest90(DrivetrainSubsystem drivetrainSubsystem,
         DoubleSupplier translationXSupplier,
         DoubleSupplier translationYSupplier,
         DoubleSupplier armHeight,
-        DoubleSupplier actuatorHeight){
+        DoubleSupplier actuatorHeight,
+        DoubleSupplier gunnerSupplier){
         
-        this.drivetrain = drivetrainSubsystem;
-        this.x = translationXSupplier;
-        this.y = translationYSupplier;
+        this.m_drivetrainSubsystem = drivetrainSubsystem;
+        this.m_translationXSupplier = translationXSupplier;
+        this.m_translationYSupplier = translationYSupplier;
         this.armHeight = armHeight;
         this.actuatorHeight = actuatorHeight;
+        this.gunnerSupplier = gunnerSupplier;
+
         addRequirements(drivetrainSubsystem);  
 
     }
     @Override
     public void initialize(){
         controller.enableContinuousInput(-180, 180);
-        axis = (int) drivetrain.getRotation2d().getDegrees() / 90;
-        if(drivetrain.getRotation2d().getDegrees() - axis * 90 > 45) axis++ ;
+        axis = (int) m_drivetrainSubsystem.getRotation2d().getDegrees() / 90;
+        if(m_drivetrainSubsystem.getRotation2d().getDegrees() - axis * 90 > 45) axis++ ;
         desiredAngle = axis * 90;
     }
-
     @Override
-    public void execute(){
-        lastRot = drivetrain.getRotation2d().getDegrees();
-        axis = ( (int) lastRot) / 90 -1;
-        if(lastRot - axis * 90 > 45) axis++ ;
-        desiredAngle = axis * 90;
+    public void execute() {
+        SmartDashboard.putNumber("desiredAngle", desiredAngle);
+        m_translationXTrapezoidal = (m_translationXSupplier.getAsDouble()-m_translationXTrapezoidal)/6 + m_translationXTrapezoidal;
+        m_translationYTrapezoidal = (m_translationYSupplier.getAsDouble()-m_translationYTrapezoidal)/6 + m_translationYTrapezoidal;
 
-        xT = (x.getAsDouble()-xT)/5 + xT;
-        yT = (y.getAsDouble()-yT)/5 + yT;
+        double magnitude = Math.hypot(m_translationXTrapezoidal, m_translationYTrapezoidal);
 
-        double magnitude = Math.hypot(xT, yT);
-
-        double joyAngle = Math.atan2(yT, xT);
-        double roboAngle = (drivetrain.getNavHeading()+ joyAngle); 
+        double joyAngle = Math.atan2(m_translationYTrapezoidal, m_translationXTrapezoidal);
+        double roboAngle = (m_drivetrainSubsystem.getNavHeading() + joyAngle); 
 
         double resultX = Math.cos(roboAngle) * magnitude;
         double resultY = Math.sin(roboAngle) * magnitude;
         
-        double pidValue = controller.calculate(lastRot, desiredAngle) * Math.PI/180;
+        
+        SmartDashboard.putNumber("m_translationXSupplier", m_translationXTrapezoidal);
+        SmartDashboard.putNumber("m_translationYSupplier", m_translationYTrapezoidal);
+        SmartDashboard.putNumber("Magnitude", magnitude);
         double armY = armHeight.getAsDouble();
-        // double minArmHeight = 0.3;
-        // double maxArmHeight = 1.18;
-        // double iLerp = (armY - minArmHeight) /(minArmHeight - maxArmHeight);
-        // double divider  = Math.max(0.0, Math.min(1.0, iLerp)) * 2.5;
+        lastRot = m_drivetrainSubsystem.getRotation2d().getDegrees();
+        axis = ( (int) lastRot) / 90 -1;
+        if(lastRot - axis * 90 > 45) axis++ ;
+        desiredAngle = axis * 90;
+        double pidValue = controller.calculate(lastRot, desiredAngle) * Math.PI/180;
 
-        if(armY > Constants.ArmNodeDictionary.ready_midcube_score_y){      
-            if(armHeight.getAsDouble() > minArmValue) {//start slow                 Constants.ArmNodeDictionary.ready_midcube_score_y){
-                // double iLerp = (armY - minArmValue) / (minArmValue - maxArmValue); //inverse linear interpolation to get from 0 to 1 between min and max arm heights
-                // double speedMultiplier = 1 + Math.max(0.0, Math.min(1.0, iLerp)) * (1.5); //clamp it actually between 0 to 1 in case oops! incident
-                drivetrain.drive(
+        if(Math.abs(gunnerSupplier.getAsDouble()) < 2){
+         if(armY > 0.35){
+            m_drivetrainSubsystem.drive(
                         // ChassisSpeeds.fromFieldRelativeSpeeds(
-                        new ChassisSpeeds(
-                        resultX/2.5,
-                        resultY/2.5,
-                        pidValue)
+                new ChassisSpeeds(
+                resultX/2.25,
+                resultY/2.25,
+                pidValue
+                )
                         // m_drivetrainSubsystem.getRotation2d()
                         // )
                 );
          }
-        }
-    else if(actuatorHeight.getAsDouble() > 50){
-        drivetrain.drive(
-            // ChassisSpeeds.fromFieldRelativeSpeeds(
-            new ChassisSpeeds(
-            resultX/1.5,
-            resultY/1.5,
-            pidValue)
-            // m_drivetrainSubsystem.getRotation2d()
-            // )
-        );
-            }
-        else{
-            drivetrain.drive(
-                    // ChassisSpeeds.fromFieldRelativeSpeeds(
-                    new ChassisSpeeds(
-                    resultX,
-                    resultY,
-                    pidValue)
-                    // m_drivetrainSubsystem.getRotation2d()
-                    // )
+        else if(actuatorHeight.getAsDouble() > 50){
+
+            m_drivetrainSubsystem.drive(
+                // ChassisSpeeds.fromFieldRelativeSpeeds(
+                new ChassisSpeeds(
+                resultX/2,
+                resultY/2,
+                pidValue)
+                // m_drivetrainSubsystem.getRotation2d()
+                // )
             );
+                }
+            else{
+                m_drivetrainSubsystem.drive(
+                        // ChassisSpeeds.fromFieldRelativeSpeeds(
+                        new ChassisSpeeds(
+                        resultX,
+                        resultY,
+                        pidValue)
+                        // m_drivetrainSubsystem.getRotation2d()
+                        // )
+                );
+            }
         }
-    }
+            else{
+                if(armY > 0.35 ){
+                        if(Math.abs(ArmNodeDictionary.ready_double_substation_y - armY) < 0.03 )
+                            m_drivetrainSubsystem.drive(
+                                    // ChassisSpeeds.fromFieldRelativeSpeeds(
+                                    new ChassisSpeeds(
+                                    resultX/2.25,
+                                    Math.copySign(0.5, gunnerSupplier.getAsDouble()),
+                                    pidValue)
+                                    // m_drivetrainSubsystem.getRotation2d()
+                                    // )
+                        );
+                        else{
+                            m_drivetrainSubsystem.drive(
+                                    // ChassisSpeeds.fromFieldRelativeSpeeds(
+                                    new ChassisSpeeds(
+                                    resultX/2.25,
+                                    Math.copySign(0.35, gunnerSupplier.getAsDouble()),
+                                    pidValue)
+                        );
+                        }
+
+                 }
+                else if(actuatorHeight.getAsDouble() > 50){
+                    m_drivetrainSubsystem.drive(
+                        // ChassisSpeeds.fromFieldRelativeSpeeds(
+                        new ChassisSpeeds(
+                        resultX/2,
+                        Math.copySign(0.35, gunnerSupplier.getAsDouble()),
+                        pidValue)
+                        // m_drivetrainSubsystem.getRotation2d()
+                        // )
+                    );
+                        }
+                    else{
+                        m_drivetrainSubsystem.drive(
+                                // ChassisSpeeds.fromFieldRelativeSpeeds(
+                                new ChassisSpeeds(
+                                resultX,
+                                Math.copySign(0.35, gunnerSupplier.getAsDouble()),
+                                pidValue)
+                                // m_drivetrainSubsystem.getRotation2d()
+                                // )
+                        );
+                    }
+            }
+        }
 
     @Override
     public void end(boolean interrupted){
-        drivetrain.drive(new ChassisSpeeds(0.0,0.0,0.0));
+        m_drivetrainSubsystem.drive(new ChassisSpeeds(0.0,0.0,0.0));
     }
 
     @Override
