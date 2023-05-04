fca1047392eff6b74b5e5d22a458ee13b6559452
sjwang05 <63834813+sjwang05@users.noreply.github.com>
2023-05-04 10:45:22 -0700

    Revert "Revert "Revert "Revert "Change copies to references""""
    This reverts commit 335991d7aa306d0b49e7d9a88b24c9244b7cdb79.

--- re-fchangs/rust/src/particle.rs
+++ re-fchangs/rust/src/particle.rs
@@ -12,19 +12,19 @@ impl Particle {
         Particle(0.0, 0.0, 0.0, 0.0)
     }
 
-    pub fn x(&self) -> f64 {
-        self.0
+    pub fn x(&mut self) -> &mut f64 {
+        &mut self.0
     }
 
-    pub fn y(&self) -> f64 {
-        self.1
+    pub fn y(&mut self) -> &mut f64 {
+        &mut self.1
     }
 
-    pub fn w(&self) -> f64 {
-        self.2
+    pub fn w(&mut self) -> &mut f64 {
+        &mut self.2
     }
 
-    pub fn weight(&self) -> f64 {
-        self.3
+    pub fn weight(&mut self) -> &mut f64 {
+        &mut self.3
     }
 }
